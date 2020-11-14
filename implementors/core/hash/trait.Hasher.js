82901bdc26c7cfe4cba4310421143f3084eaa19f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-14 21:37:55 +0000

    Deploy telegram-rs/telegram-bot to github.com/telegram-rs/telegram-bot.git:gh-pages

---
+++ implementors/core/hash/trait.Hasher.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["fnv"] = [{"text":"impl Hasher for FnvHasher","synthetic":false,"types":[]}];
+implementors["siphasher"] = [{"text":"impl Hasher for SipHasher","synthetic":false,"types":[]},{"text":"impl Hasher for SipHasher13","synthetic":false,"types":[]},{"text":"impl Hasher for SipHasher24","synthetic":false,"types":[]},{"text":"impl Hasher for SipHasher","synthetic":false,"types":[]},{"text":"impl Hasher for SipHasher13","synthetic":false,"types":[]},{"text":"impl Hasher for SipHasher24","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/serde/de/trait.IntoDeserializer.js
@@ -0,0 +1,5 @@
+(function() {var implementors = {};
+implementors["serde"] = [];
+implementors["serde_json"] = [{"text":"impl&lt;'de&gt; IntoDeserializer&lt;'de, Error&gt; for Value","synthetic":false,"types":[]}];
+implementors["serde_value"] = [{"text":"impl&lt;'de&gt; IntoDeserializer&lt;'de, DeserializerError&gt; for Value","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
